fbe90b560807e377735f2624003c3123996b774b
Adithya Bharadwaj <adithyabharadwaj@Adithya.local>
2015-08-19 17:08:27 +0530

    Initial Commit

---
+++ slidemenuMMDrawer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slidemenuMMDrawer
+//
+//  Created by Adithya Bharadwaj on 19/08/15.
+//  Copyright (c) 2015 Adithya Bharadwaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
